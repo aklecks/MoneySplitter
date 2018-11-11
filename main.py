6659d81650993f37f115846d738f975211515cec
aklecks <a-klecks@hotmail.de>
2018-11-11 22:55:25 +0100

    endlich

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from help_functions import *
 from quickmaffs import *
 
 filepath = 'Data/bigdata.csv'
-create_data(1000, 5)
+create_data(5000, 5)
 data = load_data(filepath)
 
 quickmaffs(data)  # TODO: funktioniert oft nicht, wenn mehr als 5 spalten
