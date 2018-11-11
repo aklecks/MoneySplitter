f98798168248eb2a4125bcee2d2748f9ad214d5d
aklecks <a-klecks@hotmail.de>
2018-11-11 22:06:19 +0100

    endlich

--- quickmaffs.py
+++ quickmaffs.py
@@ -40,32 +40,51 @@ def quickmaffs(data):
             print('!!FATAL ERROR!!')
             break
         if sortiert[a] + sortiert[ende] > 0:
-            # runden auf 2 dezimalstellen
+            # zu überweisender betrag gerundet auf 2 dezimalstellen
             differenz_rounded = -1 * np.around(differenz[oldpos[a]], decimals=2)
+            # ausgabe
             print("Person " + str(oldpos[a]) + " -> Person " + str(oldpos[ende]) + ": " + str(differenz_rounded))
+            # änderung der übrigen beträge und sortierung
             differenz[oldpos[ende]] += differenz[oldpos[a]]
-            differenz[oldpos[a]] = 0
+            v0 = differenz[oldpos[ende]]
+            differenz[oldpos[a]] = 0.0
             sortiert = sorted(differenz)
+            # permutierung von oldpos: erst 0-tes Element an die richtige Stelle, dann Endelement
             p0 = oldpos[0]
             oldpos = np.delete(oldpos, 0)
             oldpos = np.insert(oldpos, sortiert.index(0), p0)
+            pe = oldpos[ende]
+            oldpos = np.delete(oldpos, ende)
+            oldpos = np.insert(oldpos, sortiert.index(v0), pe)
 
         elif sortiert[a] + sortiert[ende] < 0:
-            sortiert_rounded = np.around(sortiert[ende], decimals=2)
-            print(("Person " + str(oldpos[a]) + " -> Person " + str(oldpos[ende]) + ": " + str(sortiert_rounded)))
-            sortiert[ende] = 0
-            sortiert[a] += sortiert[ende]
-            sortiert = sorted(sortiert)
+            # zu überweisender betrag gerundet auf 2 dezimalstellen
+            differenz_rounded = np.around(differenz[oldpos[ende]], decimals=2)
+            # ausgabe
+            print(("Person " + str(oldpos[a]) + " -> Person " + str(oldpos[ende]) + ": " + str(differenz_rounded)))
+            # änderung der übrigen beträge und sortierung
+            differenz[oldpos[a]] += differenz[oldpos[ende]]
+            v0 = differenz[oldpos[a]]
+            differenz[oldpos[ende]] = 0.0
+            sortiert = sorted(differenz)
+            # permutierung von oldpos: erst Endelement an die richtige Stelle, dann 0-element
             pe = oldpos[ende]
             oldpos = np.delete(oldpos, ende)
             oldpos = np.insert(oldpos, sortiert.index(0), pe)
+            p0 = oldpos[0]
+            oldpos = np.delete(oldpos, 0)
+            oldpos = np.insert(oldpos, sortiert.index(v0), p0)
 
         elif sortiert[a] + sortiert[ende] == 0 and sortiert[a] < 0:
-            sortiert_rounded = np.around(sortiert[ende], decimals=2)
-            print(("Person " + str(oldpos[a]) + " -> Person " + str(oldpos[ende]) + ": " + str(sortiert_rounded)))
-            sortiert[ende] = 0
-            sortiert[0] = 0
+            # zu überweisender betrag gerundet auf 2 dezimalstellen
+            differenz_rounded = np.around(differenz[oldpos[ende]], decimals=2)
+            # ausgabe
+            print(("Person " + str(oldpos[a]) + " -> Person " + str(oldpos[ende]) + ": " + str(differenz_rounded)))
+            # änderung der jeweiligen Beträge auf 0
+            sortiert[ende] = 0.0
+            sortiert[a] = 0.0
             sortiert = sorted(sortiert)
+            # permutierung von oldpos
             p0 = oldpos[0]
             pe = oldpos[ende]
             oldpos = np.delete(oldpos, 0)
