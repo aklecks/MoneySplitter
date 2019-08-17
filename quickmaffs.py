d2d8c54d5077ca3f18f867abfe708be334a452ba
aklecks <a-klecks@hotmail.de>
2019-08-17 16:38:00 +0200

    update

--- quickmaffs.py
+++ quickmaffs.py
@@ -11,89 +11,89 @@ def quickmaffs(data):
     num_persons = data.shape[1]
     sums_list = [sum(data[names_list[i]]) for i in range(num_persons)]
     mean = np.mean(sums_list)
-    difference = sums_list - mean  # TODO this is casted into numpy array, since mean is interpreted as numpy array
-    difference2 = difference.copy()
-    sortiert = sorted(difference)
+    difference_list = sums_list - mean  # TODO this is casted into numpy array, since mean is interpreted as numpy array
+    difference_list2 = difference_list.copy()
+    sorted_difference_list = sorted(difference_list)
 
     # Output of the basics
     print('Personenzahl: %s\nDurschnitt der Ausgaben: %f' % (num_persons, mean))
 
-    # welche person ist sortiert[i]
-    oldpos = np.array([list(difference).index(sortiert[i]) for i in range(num_persons)])
+    # welche person ist sorted_difference_list[i]
+    oldpos = np.array([list(difference_list).index(sorted_difference_list[i]) for i in range(num_persons)])
 
     # richtige numerierung von leuten die gleichviel gezahlt haben
     i = 0
     while i in range(num_persons - 2):
-        gleichzahlig = list(oldpos[i] - oldpos).count(0)
-        for a in range(i, i + gleichzahlig):
-            difference2[oldpos[a]] += np.pi + a
-            oldpos[a + 1] = list(difference2).index(sortiert[a + 1])
-        i += 1 + gleichzahlig
+        count_same_expenses = list(oldpos[i] - oldpos).count(0)
+        for a in range(i, i + count_same_expenses):
+            difference_list2[oldpos[a]] += np.pi + a
+            oldpos[a + 1] = list(difference_list2).index(sorted_difference_list[a + 1])
+        i += 1 + count_same_expenses
 
     # calculation
     a = 0
-    ende = num_persons - 1
-    differenz_rounded = 1  # just initialization
+    end = num_persons - 1
+    differece_rounded = 1  # just initialization
     loop_count = 0  # to count the number of loops
     print('\nDie folgenden Transaktionen werden vorgeschlagen:\n\n' + '-'*50)
-    while sortiert.count(0) <= num_persons - 2:
-        if loop_count > ende:  # avoid endless loop
+    while sorted_difference_list.count(0) <= num_persons - 2:
+        if loop_count > end:  # avoid endless loop
             print('!!FATAL ERROR!!')
             break
-        if sortiert[a] + sortiert[ende] > 0:
+        if sorted_difference_list[a] + sorted_difference_list[end] > 0:
             # zu überweisender betrag gerundet auf 2 dezimalstellen
-            differenz_rounded = -1 * np.around(difference[oldpos[a]], decimals=2)
+            differece_rounded = -1 * np.around(difference_list[oldpos[a]], decimals=2)
             # ausgabe
-            print(names_list[oldpos[a]] + " -> " + names_list[oldpos[ende]] + ": " + str(differenz_rounded))
+            print(names_list[oldpos[a]] + " -> " + names_list[oldpos[end]] + ": " + str(differece_rounded))
             # änderung der übrigen beträge und sortierung
-            difference[oldpos[ende]] += difference[oldpos[a]]
-            v0 = difference[oldpos[ende]]
-            difference[oldpos[a]] = 0.0
-            sortiert = sorted(difference)
+            difference_list[oldpos[end]] += difference_list[oldpos[a]]
+            v0 = difference_list[oldpos[end]]
+            difference_list[oldpos[a]] = 0.0
+            sorted_difference_list = sorted(difference_list)
             # permutierung von oldpos: erst 0-tes Element an die richtige Stelle, dann Endelement
             p0 = oldpos[0]
             oldpos = np.delete(oldpos, 0)
-            oldpos = np.insert(oldpos, sortiert.index(0), p0)
-            pe = oldpos[ende]
-            oldpos = np.delete(oldpos, ende)
-            oldpos = np.insert(oldpos, sortiert.index(v0), pe)
+            oldpos = np.insert(oldpos, sorted_difference_list.index(0), p0)
+            pe = oldpos[end]
+            oldpos = np.delete(oldpos, end)
+            oldpos = np.insert(oldpos, sorted_difference_list.index(v0), pe)
 
-        elif sortiert[a] + sortiert[ende] < 0:
+        elif sorted_difference_list[a] + sorted_difference_list[end] < 0:
             # zu überweisender betrag gerundet auf 2 dezimalstellen
-            differenz_rounded = np.around(difference[oldpos[ende]], decimals=2)
+            differece_rounded = np.around(difference_list[oldpos[end]], decimals=2)
             # ausgabe
-            print((names_list[oldpos[a]] + " -> " + names_list[oldpos[ende]] + ": " + str(differenz_rounded)))
+            print((names_list[oldpos[a]] + " -> " + names_list[oldpos[end]] + ": " + str(differece_rounded)))
             # änderung der übrigen beträge und sortierung
-            difference[oldpos[a]] += difference[oldpos[ende]]
-            v0 = difference[oldpos[a]]
-            difference[oldpos[ende]] = 0.0
-            sortiert = sorted(difference)
+            difference_list[oldpos[a]] += difference_list[oldpos[end]]
+            v0 = difference_list[oldpos[a]]
+            difference_list[oldpos[end]] = 0.0
+            sorted_difference_list = sorted(difference_list)
             # permutierung von oldpos: erst Endelement an die richtige Stelle, dann 0-element
-            pe = oldpos[ende]
-            oldpos = np.delete(oldpos, ende)
-            oldpos = np.insert(oldpos, sortiert.index(0), pe)
+            pe = oldpos[end]
+            oldpos = np.delete(oldpos, end)
+            oldpos = np.insert(oldpos, sorted_difference_list.index(0), pe)
             p0 = oldpos[0]
             oldpos = np.delete(oldpos, 0)
-            oldpos = np.insert(oldpos, sortiert.index(v0), p0)
+            oldpos = np.insert(oldpos, sorted_difference_list.index(v0), p0)
 
-        elif sortiert[a] + sortiert[ende] == 0 and sortiert[a] < 0:
+        elif sorted_difference_list[a] + sorted_difference_list[end] == 0 and sorted_difference_list[a] < 0:
             # zu überweisender betrag gerundet auf 2 dezimalstellen
-            differenz_rounded = np.around(difference[oldpos[ende]], decimals=2)
+            differece_rounded = np.around(difference_list[oldpos[end]], decimals=2)
             # ausgabe
-            print((names_list[oldpos[a]] + " -> " + names_list[oldpos[ende]] + ": " + str(differenz_rounded)))
+            print((names_list[oldpos[a]] + " -> " + names_list[oldpos[end]] + ": " + str(differece_rounded)))
             # änderung der jeweiligen Beträge auf 0
-            difference[oldpos[ende]] = 0.0
-            difference[oldpos[a]] = 0.0
-            sortiert = sorted(difference)
+            difference_list[oldpos[end]] = 0.0
+            difference_list[oldpos[a]] = 0.0
+            sorted_difference_list = sorted(difference_list)
             # permutierung von oldpos
             p0 = oldpos[0]
-            pe = oldpos[ende]
+            pe = oldpos[end]
             oldpos = np.delete(oldpos, 0)
-            oldpos = np.insert(oldpos, sortiert.index(0), p0)
-            oldpos = np.delete(oldpos, ende)
-            oldpos = np.insert(oldpos, sortiert.index(0), pe)
+            oldpos = np.insert(oldpos, sorted_difference_list.index(0), p0)
+            oldpos = np.delete(oldpos, end)
+            oldpos = np.insert(oldpos, sorted_difference_list.index(0), pe)
 
-        if differenz_rounded < 0.005:
+        if differece_rounded < 0.005:
             break
 
         loop_count += 1
