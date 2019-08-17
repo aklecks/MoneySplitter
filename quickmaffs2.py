a1baaa679ee032dee84fd2ec035bcc3f03315299
aklecks <a-klecks@hotmail.de>
2019-08-17 18:11:32 +0200

    update

--- quickmaffs2.py
+++ quickmaffs2.py
@@ -16,16 +16,18 @@ def quickmaffs2(data):
     print('Personenzahl: %s\nDurschnitt der Ausgaben: %f' % (num_persons, mean))
     print('\nDie folgenden Transaktionen werden vorgeschlagen:\n\n' + '-' * 50)
     for i in range(num_persons - 1):
+        maxpos = np.argmax(difference_array)
+        minpos = np.argmin(difference_array)
         if - difference_array.min() <= difference_array.max():
             difference_rounded = -1 * np.around(difference_array.min(), decimals=2)
-            print('%s --> %s: %.2f' % (names_list[np.argmin(difference_array)], names_list[np.argmax(difference_array)], difference_rounded))
-            difference_array[np.argmax(difference_array)] += difference_array.min()
-            difference_array[np.argmin(difference_array)] = 0
+            print('%s --> %s: %.2f' % (names_list[minpos], names_list[maxpos], difference_rounded))
+            difference_array[maxpos] += difference_array.min()
+            difference_array[minpos] = 0
         else:
             difference_rounded = np.around(difference_array.max(), decimals=2)
-            print('%s --> %s: %.2f' % (names_list[np.argmin(difference_array)], names_list[np.argmax(difference_array)], difference_rounded))
-            difference_array[np.argmin(difference_array)] += difference_array.max()
-            difference_array[np.argmax(difference_array)] = 0
+            print('%s --> %s: %.2f' % (names_list[minpos], names_list[maxpos], difference_rounded))
+            difference_array[minpos] += difference_array.max()
+            difference_array[maxpos] = 0
         i += 1
 
     stop = default_timer()
