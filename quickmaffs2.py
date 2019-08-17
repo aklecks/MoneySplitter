e12e2bc98d64eae9b9bcd4b12f7885fb2356317f
aklecks <a-klecks@hotmail.de>
2019-08-17 17:59:52 +0200

    update

--- quickmaffs2.py
+++ quickmaffs2.py
@@ -14,17 +14,18 @@ def quickmaffs2(data):
     difference_array = np.array(sums_list - mean)
 
     print('Personenzahl: %s\nDurschnitt der Ausgaben: %f' % (num_persons, mean))
-
-    if - difference_array.min() <= difference_array.max():
-        difference_rounded = -1 * np.around(difference_array.min(), decimals=2)
-        print('%s --> %s: %f' % (names_list[np.argmin(difference_array)], names_list[np.argmax(difference_array)], difference_rounded))
-        difference_array[np.argmax(difference_array)] += difference_array.min()
-        difference_array[np.argmin(difference_array)] = 0
-    else:
-        difference_rounded = np.around(difference_array.max(), decimals=2)
-        print('%s --> %s: %f' % (names_list[np.argmin(difference_array)], names_list[np.argmax(difference_array)], difference_rounded))
-        difference_array[np.argmin(difference_array)] += difference_array.max()
-        difference_array[np.argmax(difference_array)] = 0
+    for i in range num_persons-1
+        if - difference_array.min() <= difference_array.max():
+            difference_rounded = -1 * np.around(difference_array.min(), decimals=2)
+            print('%s --> %s: %f' % (names_list[np.argmin(difference_array)], names_list[np.argmax(difference_array)], difference_rounded))
+            difference_array[np.argmax(difference_array)] += difference_array.min()
+            difference_array[np.argmin(difference_array)] = 0
+        else:
+            difference_rounded = np.around(difference_array.max(), decimals=2)
+            print('%s --> %s: %f' % (names_list[np.argmin(difference_array)], names_list[np.argmax(difference_array)], difference_rounded))
+            difference_array[np.argmin(difference_array)] += difference_array.max()
+            difference_array[np.argmax(difference_array)] = 0
+        i += 1
 
     stop = default_timer()
     print('-' * 50 + '\n\ncalculating took ', stop - start, ' seconds\n')
