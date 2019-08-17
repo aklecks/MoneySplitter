1d9985c9d8cd035dd5b38634900294d4eb7f7dbb
Christian Schlarmann <christian.schlarmann@web.de>
2019-08-17 15:39:50 +0200

    - leave out columns with no name when reading data (for trans. purpose) - rename some variables

--- help_functions.py
+++ help_functions.py
@@ -8,18 +8,21 @@ def load_data(filepath='data/testdata.csv'):
     load the data from csv file
     """
     print('loading data..')
+    # get the column names (so we can drop those that we don't want)
+    columns = list(pd.read_csv(filepath, nrows=1))
+    # we don't want columns with empty names (since these are used for transaction purpose)
+    columns_we_want = [col for col in columns if 'Unnamed' not in col]
     # read the data from file
-    data = pd.read_csv(filepath, dtype=float)
+    data = pd.read_csv(filepath, usecols=columns_we_want, dtype=float)
     # fill in NaN values with zero
     data.fillna(0, inplace=True)
 
+    # print it out
     print('data: \n', data)
-    #print('erste spalte: \n', data['Person1'])
-    #print('erste spalte zweiter eintrag: \n', data['Person1'][1])
     return data
 
 
-def create_data(no_columns=5, no_rows=10, filename='bigdata.csv', use_real_names=True):
+def create_data(num_persons, num_transactions, filename='bigdata.csv', use_real_names=True):
     """
     create a csv file with no_columns and no_rows
     """
@@ -30,14 +33,14 @@ def create_data(no_columns=5, no_rows=10, filename='bigdata.csv', use_real_names
         # random names
         columns = ['Jaqueline', 'Chantal', 'Atomfried', 'Margarett', 'Jeana', 'Flavia', 'Kattie', 'Kendrick', 'Meghann', 'Sherika']
         # set number of columns
-        no_columns = len(columns)
+        num_persons = len(columns)
         # tell the user
-        print('Attention: in this mode, the number of columns has to be exactly %i' % no_columns)
+        print('Attention: in this mode, the number of columns has to be exactly %i' % num_persons)
     else:
-        columns = ['Person' + str(i) for i in range(no_columns)]
+        columns = ['Person' + str(i) for i in range(num_persons)]
 
     # create array with random numbers
-    random_array = np.random.randn(no_rows, no_columns)*40
+    random_array = np.random.randn(num_transactions, num_persons) * 40
     random_array = np.absolute(random_array)  # we want only positive values
     # make pandas dataframe
     data = pd.DataFrame(random_array, columns=columns)
