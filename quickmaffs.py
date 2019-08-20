75e765b94c88d4c616decae163ef20adb61f172a
Christian Schlarmann <christian.schlarmann@web.de>
2019-08-20 14:12:27 +0200

    - write output to txt file

--- quickmaffs.py
+++ quickmaffs.py
@@ -2,7 +2,7 @@ import numpy as np
 from timeit import default_timer
 
 
-def quickmaffs(data):
+def quickmaffs(data, output_file='data/results.txt'):
     print('calculating..')
     start = default_timer()  # to time the function
     names_list = list(data.columns)  # lists the names of participants
@@ -10,9 +10,9 @@ def quickmaffs(data):
     sums_list = list(data.loc['Sum'])  # sums of spent money (same index as names)
     mean = np.mean(sums_list)  # mean value of spent money
     difference_array = np.array(sums_list - mean)  # difference between spent money and mean value for each person
-
-    print('Numper of participants: %s\nMean of the expenses: %.2f' % (num_persons, float(mean)))
-    print('\nThe following transactions are recommended:\n\n' + '-' * 50)
+    # string for all the output
+    output_str = 'Numper of participants: %s\nMean of the expenses: %.2f' % (num_persons, float(mean))
+    output_str += '\nThe following transactions are recommended:\n\n' + '-' * 50 + '\n'
 
     for i in range(num_persons - 1):
         maxpos = int(np.argmax(difference_array))  # position of the person who spent the most (after transaction)
@@ -24,14 +24,23 @@ def quickmaffs(data):
             break
         # case differentiation to see which person's sums will be changed by the transaction
         if - difference_array.min() <= difference_array.max():
-            print('%s --> %s: %.2f' % (names_list[minpos], names_list[maxpos], difference_rounded))
+            output_str += '%s --> %s: %.2f\n' % (names_list[minpos], names_list[maxpos], difference_rounded)
             # transaction
             difference_array[maxpos] += difference_array.min()
             difference_array[minpos] = 0
         else:
-            print('%s --> %s: %.2f' % (names_list[minpos], names_list[maxpos], difference_rounded))
+            output_str += '%s --> %s: %.2f\n' % (names_list[minpos], names_list[maxpos], difference_rounded)
             difference_array[minpos] += difference_array.max()
             difference_array[maxpos] = 0
 
     stop = default_timer()
-    print('-' * 50 + '\n\ncalculating took %.8f seconds' % (stop - start))
+
+    output_str += '-' * 50 + '\n\ncalculating took %.8f seconds' % (stop - start)
+    print(output_str)
+
+    if output_file is not None:
+        # write to txt file
+        with open(output_file, 'w') as f:
+            f.write(output_str)
+
+        print('saved output to %s\n' % output_file)
